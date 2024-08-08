4fec8172bf60b96e6b593b08b29bcf736fad90b1
Henrique Souza <h260497@dac.unicamp.br>
2024-08-08 11:04:46 -0300

    add data

---
+++ get_path.py
@@ -0,0 +1,10 @@
+import os
+
+folder_path = 'data'
+
+def get_path():
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            relative_path = os.path.relpath(file_path, folder_path)
+            print(relative_path)
